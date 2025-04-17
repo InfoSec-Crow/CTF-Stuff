12817fcbc190e2a5fae3d20331e380455f1ac318
Crow <crow@example.de>
2025-04-17 17:13:34 +0100

    ctf start commands

---
+++ ctf/ctf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import os
+import argparse
+import subprocess
+
+WS_PATH = '/home/kali/Desktop/htb/box/'
+
+parser = argparse.ArgumentParser(description='CTF start commands')
+parser.add_argument("-n","--name", required=True, type=str, help='Name')
+parser.add_argument("-i", "--ip", required=True, type=str, help='IP')
+args = parser.parse_args()
+
+def folder():
+    os.makedirs(f'{WS_PATH}{args.name}', exist_ok=True)
+    print(f'Make folder: {WS_PATH}{args.name}')
+
+def nmap():
+    # Nmap-Befehl zusammenbauen
+    nmap_command = f"nmap -v -sC -sV -oN {args.name}.nmap {args.ip}"
+
+    # Nmap-Befehl mit Popen ausführen
+    process = subprocess.Popen(nmap_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Live-Ausgabe des Befehls in Echtzeit anzeigen
+    for stdout_line in iter(process.stdout.readline, ""):
+        print(stdout_line, end="")  # Ausgeben der stdout-Zeile
+    for stderr_line in iter(process.stderr.readline, ""):
+        print(stderr_line, end="")  # Ausgeben der stderr-Zeile
+
+    process.stdout.close()
+    process.stderr.close()
+    process.wait()
+
+folder()
+os.chdir(f'{WS_PATH}{args.name}')
+nmap()
---
+++ makepl/README.md
@@ -0,0 +1,11 @@
+# Payload maker
+
+Currently available:
+- PHP
+    - cmd.php
+    - revshell.php
+- Bash
+    - revshell.sh
+- Powershell
+    - revshell.sh
+    - base64 revshell.sh
