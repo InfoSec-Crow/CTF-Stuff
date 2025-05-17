2221454171a563181a520464fcc5d7db7cf129d0
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-17 20:53:42 +0200

    Update makepl.py

--- scripts/makepl/makepl.py
+++ scripts/makepl/makepl.py
@@ -7,7 +7,12 @@ from pl.bash_pl import bash_pl
 from pl.ps_pl import ps_pl 
 
 def get_tun0_ip():
-    return subprocess.check_output("ip a | grep -A 2 'tun0:' | grep -oP '(?<=inet\\s)\\d+(\\.\\d+){3}'", shell=True).decode().strip()
+    try:
+        ip = subprocess.check_output("ip a | grep -A 2 'tun0:' | grep -oP '(?<=inet\\s)\\d+(\\.\\d+){3}'", shell=True).decode().strip()
+    except:
+        print('No tun0 - use 127.0.0.1')
+        ip = '127.0.0.1'
+    return ip
 
 parser = argparse.ArgumentParser(description='Payload maker')
 parser.add_argument("--lhost", type=str, default=get_tun0_ip(), help='local host (default: tun0)')
