77bc1eb1fd035012fad44998080a6ebe071624d3
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2025-05-17 12:38:46 +0200

    Update symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Scripts
 sudo ln -s /opt/CTF-Stuff/scripts/makepl/makepl.py /usr/local/bin/makepl
-
 sudo ln -s /opt/CTF-Stuff/scripts/server/s.py /usr/local/bin/s
+sudo ln -s /opt/CTF-Stuff/scripts/ssh_/ssh_.py /usr/local/bin/ssh_
 
+
+# Setup
 sudo ln -s /opt/CTF-Stuff/setup/ctf/start_ctf.py /usr/local/bin/start_ctf
